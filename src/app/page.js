741435202b7b876dceab75d0865f677377ea5dc7
rojanstha621 <rojanstha621@gmail.com>
2025-06-18 08:00:24 +0545

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -39,8 +39,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
-          </a>
+        Hi we can deply this </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -65,7 +64,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Learn to code with Node.js
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
